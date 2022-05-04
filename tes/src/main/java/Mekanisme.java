1a9c07c937048767b86a2a6a528014d14870a8fd
Nicholas Arsa <89065410+AscendingGrass@users.noreply.github.com>
2022-05-04 21:13:36 +0700

    ngubah sout
    hello world jadi ruben kontol

--- tes/src/main/java/Mekanisme.java
+++ tes/src/main/java/Mekanisme.java
@@ -1,5 +1,6 @@
 public class Mekanisme {
     public Mekanisme(){
-        System.out.println("Hello World!");
+        System.out.println("Hello monyet!");
+        System.out.println("ruben kontol");
     }
 }
