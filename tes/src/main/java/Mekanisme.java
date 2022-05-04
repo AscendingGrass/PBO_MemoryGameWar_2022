ce741eb4d0f3147e8e0a5ed5d41cb829f2675ed0
rubenyw <rubenyasonwinarta@gmail.com>
2022-05-04 21:30:45 +0700

    Update Mekanisme
    gk ada sg di ubah seh mek benerin class

--- tes/src/main/java/Mekanisme.java
+++ tes/src/main/java/Mekanisme.java
@@ -8,10 +8,10 @@
  *
  * @author user
  */
-public class Mekanisme {
-    // PENTING YA CLASS INI PERLU BANGET CLASS NAMANYA MEKANISME JANGAN DI DELETE
+
 public class Mekanisme {
     public Mekanisme(){
+        // PENTING YA CLASS INI PERLU BANGET CLASS NAMANYA MEKANISME JANGAN DI DELETE
         System.out.println("Simon Kntl");
     }
 }
