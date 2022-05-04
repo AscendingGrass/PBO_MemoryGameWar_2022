23a7fe7f72f9a8b54b2829c2da0931e3546560cd
rubenyw <rubenyasonwinarta@gmail.com>
2022-05-04 21:32:51 +0700

    Hapus comment
    cuma hapus comment tidak penting

--- tes/src/main/java/Mekanisme.java
+++ tes/src/main/java/Mekanisme.java
@@ -1,14 +1,3 @@
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author user
- */
-
 public class Mekanisme {
     public Mekanisme(){
         // PENTING YA CLASS INI PERLU BANGET CLASS NAMANYA MEKANISME JANGAN DI DELETE
