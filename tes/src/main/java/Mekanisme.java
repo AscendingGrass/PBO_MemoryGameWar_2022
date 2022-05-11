19db56f911a54bf11cad32327ff7e59de2ccf5a2
rubenyw <rubenyasonwinarta@gmail.com>
2022-05-11 08:51:03 +0700

    test beans
    hbs uninstall netbeans

--- tes/src/main/java/Mekanisme.java
+++ tes/src/main/java/Mekanisme.java
@@ -2,5 +2,6 @@ public class Mekanisme {
     public Mekanisme(){
         // PENTING YA CLASS INI PERLU BANGET CLASS NAMANYA MEKANISME JANGAN DI DELETE
         System.out.println("Simon Kntl");
+        System.out.println("Test netbeans baru");
     }
 }
