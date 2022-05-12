81dc6fc2fdca1235c75fc0e164a279420d8b2f95
rubenyw <rubenyasonwinarta@gmail.com>
2022-05-12 17:07:31 +0700

    Update Mekanisme.java

--- tes/src/main/java/Mekanisme.java
+++ tes/src/main/java/Mekanisme.java
@@ -1,7 +1,6 @@
 public class Mekanisme {
     public Mekanisme(){
         // PENTING YA CLASS INI PERLU BANGET CLASS NAMANYA MEKANISME JANGAN DI DELETE
-        System.out.println("Simon Kntl");
-        System.out.println("Test netbeans baru");
+        
     }
 }
