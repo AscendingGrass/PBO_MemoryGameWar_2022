0a8a9ef171f1b86f59a6fe7bed3d51eb5e933c3e
rubenyw <rubenyasonwinarta@gmail.com>
2022-05-16 22:12:56 +0700

    Update Player.java

--- MemoryGameWar/src/Player.java
+++ MemoryGameWar/src/Player.java
@@ -9,5 +9,16 @@ public class Player {
         sp+=2;
     }
     
+    public void receiveDmg(int dmg){
+        if(hp - dmg >= 0)hp -= dmg;
+        else hp = 0;
+    }
+    public void attack(Player p, Skill x){
+        //p.receiveDmg(x.);
+    }
+    public void useSkill(Skill x){
+        if(sp >= x.skillPoint)
+            sp -= x.skillPoint;
+    }
     
 }
